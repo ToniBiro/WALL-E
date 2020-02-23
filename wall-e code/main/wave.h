9beaa02f9f0cfd81cef77d596c2a163ee0652897
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-02-23 14:29:01 +0200

    Update wave.h

--- wall-e code/main/wave.h
+++ wall-e code/main/wave.h
@@ -1,39 +1,49 @@
 #pragma once
-// test servo on shield
-
 #include "globalVariables.h"
 
 void wave(int arm){
-  if(arm == 0){ // wave with left arm
+  
+  // wave with left arm
+  if(arm == 0){
     posServoLeftArm = 90;
     leftArm.write(posServoLeftArm);
-    for (int j = 0; j < 3; ++j){   
-      for (int i = 0; i < 80; ++i){
+
+    // wave 3 times
+    for (int j = 0; j < 3; ++j){
+
+      // move the arm down
+      for (int i = 0; i < 60; ++i){
         posServoLeftArm -= 1;
         leftArm.write(posServoLeftArm);
         delay(20);
       }
-      
-      for (int i = 0; i < 80; ++i){
+
+      // move the arm up
+      for (int i = 0; i < 60; ++i){
         posServoLeftArm += 1;
         delay(20);
       }
     }
     playOnceLeft = 0;
   }
+
+  // wave with right arm
   if(arm == 1){
     posServoRightArm = 90;
     rightArm.write(posServoRightArm);
   
-  for (int j = 0; j < 3; ++j){   
+  for (int j = 0; j < 3; ++j){
+
+     // move the arm down
       for (int i = 0; i < 60; ++i){
-        posServoRightArm -= 1;
+        posServoRightArm += 1;
         rightArm.write(posServoRightArm);
         delay(20);
       }
-      
+
+      // move the arm up
       for (int i = 0; i < 60; ++i){
-        posServoRightArm += 1;
+        posServoRightArm -= 1;
         delay(20);
       }
     }
