3ebed99614cd7bae1916f982923fee3ebfb4c5be
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-02-23 14:29:32 +0200

    Update bluetooth.h

--- wall-e code/main/bluetooth.h
+++ wall-e code/main/bluetooth.h
@@ -1,1 +1,12 @@
+#pragma once
 
+#include "globalVariables.h"
+
+// reads the commands from the bluetooth module
+void read_commands(){
+  
+  if(SUART.available()){
+    command = SUART.read();
+    Serial.println(command); //check if the connection is ok
+  }
+}
