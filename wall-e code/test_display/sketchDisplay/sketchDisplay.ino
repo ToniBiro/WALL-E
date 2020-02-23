e73c67f17b88a801f709979d8f1bb7f6dbc0c608
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-02-23 15:07:53 +0200

    Update sketchDisplay.ino

--- wall-e code/test_display/sketchDisplay/sketchDisplay.ino
+++ wall-e code/test_display/sketchDisplay/sketchDisplay.ino
@@ -1,3 +1,6 @@
+
+// we replicated the display that wall e had in the original movie
+
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <TFT_ILI9163C.h>
@@ -19,20 +22,20 @@
 #define __DC 9
 
 TFT_ILI9163C display = TFT_ILI9163C(__CS, 8, __DC);
-class point{
+class point {
   public:
     int x;
     int y;
-    point(int x = 0, int y = 0){
+    point(int x = 0, int y = 0) {
       this->x = x;
       this->y = y;
     }
-    
+
 };
 
 void drawCircle(int x, int y, int radius, int color)
 {
-  for (int i=0; i<360; i++)  
+  for (int i = 0; i < 360; i++)
   {
     double radians = i * PI / 180;
     double px = x + radius * cos(radians);
@@ -43,139 +46,145 @@ void drawCircle(int x, int y, int radius, int color)
 
 
 
-  
-// Function to find the line given two points 
-int getXOnLine(int Y, int ax, int ay, int bx, int by) 
-{ 
-    double a = by - ay; 
-    double b = ax - bx; 
-    double c = a*(ax) + b*(ay); 
-    
-    //line ecuation: ax + by = c
-    //if we want x = (c-by) / a  
-    //if we want y = (c-ax) / b
-    /*Serial.println(ax);
+
+// Function to find the line given two points
+int getXOnLine(int Y, int ax, int ay, int bx, int by)
+{
+  double a = by - ay;
+  double b = ax - bx;
+  double c = a * (ax) + b * (ay);
+
+  //line ecuation: ax + by = c
+  //if we want x = (c-by) / a
+  //if we want y = (c-ax) / b
+  /*Serial.println(ax);
     Serial.println(ay);
     Serial.println(bx);
     Serial.println(by);*/
 
-    return (c-b*Y) / a;
+  return (c - b * Y) / a;
 }
-int getYOnLine(int X, int ax, int ay, int bx, int by) 
-{ 
-    double a = by - ay; 
-    double b = ax - bx; 
-    double c = a*(ax) + b*(ay); 
-    //line ecuation: ax + by = c
-    //if we want x = (c-by) / a  
-    //if we want y = (c-ax) / b
-    return (c-a*X) / b;
+int getYOnLine(int X, int ax, int ay, int bx, int by)
+{
+  double a = by - ay;
+  double b = ax - bx;
+  double c = a * (ax) + b * (ay);
+  //line ecuation: ax + by = c
+  //if we want x = (c-by) / a
+  //if we want y = (c-ax) / b
+  return (c - a * X) / b;
 }
 
 void drawSunWaves(int x, int y, int radius, int color)
 {
   int waveLen = 5;
   int smallWaveLen = 3;
-  for (int i=5; i<360; i+=20)  
+  for (int i = 5; i < 360; i += 20)
   {
     double radians = i * PI / 180;
     double px = x + radius * cos(radians);
     double py = y + radius * sin(radians);
     //int xOnLine = getXOnLine(py + 1, px, py, x, y);
     //int yOnLine = getYOnLine(px + 1, px, py, x, y);
-    if (i> 0 && i < 85)
+    if (i > 0 && i < 85)
     {
-      
-        int yOnLine = getYOnLine(px+waveLen, px, py, x, y);
-        display.drawLine(px, py, px+waveLen, yOnLine, color);
-        /*Serial.println(py);
+
+      int yOnLine = getYOnLine(px + waveLen, px, py, x, y);
+      display.drawLine(px, py, px + waveLen, yOnLine, color);
+      /*Serial.println(py);
         Serial.println(px+1);
         Serial.println(yOnLine);*/
 
     }
-    else if(i>105 && i < 165) 
+    else if (i > 105 && i < 165)
     {
-      int yOnLine = getYOnLine(px-waveLen, px, py, x, y);
-      display.drawLine(px, py, px-waveLen, yOnLine, color);
-       
+      int yOnLine = getYOnLine(px - waveLen, px, py, x, y);
+      display.drawLine(px, py, px - waveLen, yOnLine, color);
+
     }
-    else if(i>180 &&  i< 265)
-    {  
-       int yOnLine = getYOnLine(px-waveLen, px, py, x, y);
-       display.drawLine(px, py, px-waveLen, yOnLine, color);
-    } 
-    else if(i> 285 && i < 360)
+    else if (i > 180 &&  i < 265)
     {
-       int yOnLine = getYOnLine(px+waveLen, px, py, x, y);
-        display.drawLine(px, py, px+waveLen, yOnLine, color);
+      int yOnLine = getYOnLine(px - waveLen, px, py, x, y);
+      display.drawLine(px, py, px - waveLen, yOnLine, color);
     }
-      else if(i == 285)
+    else if (i > 285 && i < 360)
+    {
+      int yOnLine = getYOnLine(px + waveLen, px, py, x, y);
+      display.drawLine(px, py, px + waveLen, yOnLine, color);
+    }
+    else if (i == 285)
     {
       smallWaveLen = 3;
-       int yOnLine = getYOnLine(px+smallWaveLen, px, py, x, y);
-        display.drawLine(px, py, px+smallWaveLen, yOnLine, color);
-    } else if(i == 265)
+      int yOnLine = getYOnLine(px + smallWaveLen, px, py, x, y);
+      display.drawLine(px, py, px + smallWaveLen, yOnLine, color);
+    } else if (i == 265)
     {
       smallWaveLen = 2;
-       int yOnLine = getYOnLine(px-smallWaveLen, px, py, x, y);
-       display.drawLine(px, py, px-smallWaveLen, yOnLine, color);
-    }else if(i == 165)
+      int yOnLine = getYOnLine(px - smallWaveLen, px, py, x, y);
+      display.drawLine(px, py, px - smallWaveLen, yOnLine, color);
+    } else if (i == 165)
     {
       smallWaveLen = 2;
-       int yOnLine = getYOnLine(px-smallWaveLen, px, py, x, y);
-       display.drawLine(px, py, px-smallWaveLen, yOnLine, color);
-    } else if(i == 105)
+      int yOnLine = getYOnLine(px - smallWaveLen, px, py, x, y);
+      display.drawLine(px, py, px - smallWaveLen, yOnLine, color);
+    } else if (i == 105)
     {
       smallWaveLen = 4;
-      int yOnLine = getYOnLine(px-smallWaveLen, px, py, x, y);
-      display.drawLine(px, py, px-smallWaveLen, yOnLine, color);
+      int yOnLine = getYOnLine(px - smallWaveLen, px, py, x, y);
+      display.drawLine(px, py, px - smallWaveLen, yOnLine, color);
     }
 
-  // down line
+    // down line
 
 
-    
+
   }
-    int i = 84;
-    double radians = i * PI / 180;
-    double px = x + radius * cos(radians);
-    double py = y + radius * sin(radians);
-    int yOnLine = getYOnLine(px+1, px, py, x, y);
-    display.drawLine(px, py, px+1, yOnLine, color);
-    
+  int i = 84;
+  double radians = i * PI / 180;
+  double px = x + radius * cos(radians);
+  double py = y + radius * sin(radians);
+  int yOnLine = getYOnLine(px + 1, px, py, x, y);
+  display.drawLine(px, py, px + 1, yOnLine, color);
+
 }
 
 void setup() {
-    Serial.begin(9600);
-    display.begin();
-    display.clearScreen();
-    display.setCursor(0,0);
-    //display.drawLine(0, 0, 50, 50, RED);
-    display.fillRect(45,100,70,20,YELLOW);
-    display.fillRect(45,90,70,5,YELLOW);
-    display.fillRect(45,80,70,5,YELLOW);
-    display.fillRect(45,70,70,5,YELLOW);
-    display.fillRect(45,60,70,5,YELLOW);
-    display.fillRect(45,50,70,5,YELLOW);
-    display.fillRect(45,40,70,5,YELLOW);
+  Serial.begin(9600);
+  display.begin();
+  display.clearScreen();
+  display.setCursor(0, 0);
 
-    drawCircle(25, 50, 12, YELLOW2);
-    drawCircle(25, 50, 11, YELLOW2);
-    drawCircle(25, 50, 10, YELLOW2);
-    //drawCircle(20, 25, 13, PALEYELLOW);
-    
-    drawCircle(25, 50, 8, PALEYELLOW2);
-    drawSunWaves(25, 50, 12, YELLOW2);
+  // draw the solar light battery level
 
-    display.setCursor(13,10);
-    display.setTextColor(YELLOW);
-    display.print("SOLAR CHARGE LEVEL");
+  //display.drawLine(0, 0, 50, 50, RED);
+  display.fillRect(45, 100, 70, 20, YELLOW);
+  display.fillRect(45, 90, 70, 5, YELLOW);
+  display.fillRect(45, 80, 70, 5, YELLOW);
+  display.fillRect(45, 70, 70, 5, YELLOW);
+  display.fillRect(45, 60, 70, 5, YELLOW);
+  display.fillRect(45, 50, 70, 5, YELLOW);
+  display.fillRect(45, 40, 70, 5, YELLOW);
+
+  // display the sun drawing from on the display
+  drawCircle(25, 50, 12, YELLOW2);
+  drawCircle(25, 50, 11, YELLOW2);
+  drawCircle(25, 50, 10, YELLOW2);
+  //drawCircle(20, 25, 13, PALEYELLOW);
+
+  drawCircle(25, 50, 8, PALEYELLOW2);
+  drawSunWaves(25, 50, 12, YELLOW2);
+
+  display.setCursor(13, 10);
+  display.setTextColor(YELLOW);
+  display.print("SOLAR CHARGE LEVEL");
 }
 
 void loop() {
-  display.fillRect(45,40,70,5,BLACK);
+
+  // turns on and off the line that shows how high the solar light level is
+  display.fillRect(45, 40, 70, 5, BLACK);
   delay(2000);
-  display.fillRect(45,40,70,5,YELLOW);
+  display.fillRect(45, 40, 70, 5, YELLOW);
   delay(2000);
 
 }
