c7482978d7031f7b4017b945d425c78cecff7b09
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-02-23 14:29:45 +0200

    add comments

--- wall-e code/main/main.ino
+++ wall-e code/main/main.ino
@@ -1,129 +1,89 @@
-
-
 #include "globalVariables.h"
 #include "arm_movement.h"
 #include "wave.h"
 #include "wheel_movement.h"
-
-
-SoftwareSerial SUART(A0, A1);
-
-const int enablePin = 11;
-const int in1Pin = 10;
-const int in2Pin = 9;
-const int btPin0 = A0;
-const int btPin1 = A1;
-
-int motorSpeed = 0;
-boolean reverse = false;
+#include "bluetooth.h"
 
 void setup() {
   
   Serial.begin(9600); // communication with the bluetooth module
   SUART.begin(9600);
-  pinMode(in1Pin, OUTPUT);
-  pinMode(in2Pin, OUTPUT);
-  pinMode(enablePin, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(btPin0, INPUT);
   pinMode(btPin1, INPUT);
   rightArm.attach(10);
   leftArm.attach(9);
   rightArm.write(180);
   leftArm.write(5);
+  
   motor1.setSpeed(220);
   motor4.setSpeed(220);
 }
 
-void setMotor(int motorSpeed , boolean reverse)
-{
-  analogWrite(enablePin, motorSpeed );
-  digitalWrite(in1Pin, ! reverse);
-  digitalWrite(in2Pin, reverse);
-}
-
-int command; // default
-
-
 void loop() {
-  if(SUART.available()){
-    command = SUART.read();
-    Serial.println(command);
-    
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
+
+  // read the command from the bluetooth module
+  read_commands();
+
+  // when the "arms" variable is 0 it means that the control is taken by the wheels
   if(arms == 0){
-    //control the weels
-    if (command == 70){
-      //setMotor(motorSpeed, reverse);
+    //control the wheels
+    if (command == upArrow){
       move_forward(1);
-      Serial.println("fata");
+      move_forward(2);
     }
   
-    if (command == 66){
-      //setMotor(motorSpeed, reverse);
+    if (command == downArrow){
       move_backward(1);
-      Serial.println("spate");
+      move_backward(2);
+    }
+
+    if(command == backLights || command == frontLights){
+      stop_moving();
     }
     
-    if (command == 82){
-      //setMotor(motorSpeed, reverse);
-      Serial.println("dreapta");
-    }
-    
-    if (command == 76){
-      //setMotor(motorSpeed, reverse);
-      Serial.println("stanga");
-    }
   }
+  // when the "arms" variable is 1 it means that the control is taken by the arms
   else{
-    //control the arms
+    
+    // first make sure the arm is not waving
+    if(playOnceLeft == 1){
+      // For the left arm we have the following mapping
+      if(command == upArrow){
+        move_left_arm_up(posServoLeftArm);
+      }
+      if(command == downArrow){
+        move_left_arm_down(posServoLeftArm);
+      }
+    }
 
-    // For the left arm
-    if(command == 70){
-      move_left_arm_up(posServoLeftArm);
-    }
-    if(command == 66){
-      move_left_arm_down(posServoLeftArm);
-    }
-
-    // For the right arm
-    if(command == 82){
-      move_right_arm_up(posServoRightArm);
-    }
-    if(command == 76){
-      move_right_arm_down(posServoRightArm);
+    if(playOnceRight == 1){
+      // For the right arm we have the following mapping
+      if(command == rightArrow){
+        move_right_arm_up(posServoRightArm);
+      }
+      if(command == rightArrow){
+        move_right_arm_down(posServoRightArm);
+      }
     }
 
     // Make Wall-e wave
     //wave with left - aka 0
-    if(command == 119 && playOnceLeft == 0){
+    if(command == frontLights && playOnceLeft == 0){
       wave(0);
       playOnceLeft = 1;
     }
     //wave with right - aka 1
-    if(command == 117 && playOnceRight == 0){
-      wave(1);
+    if(command == backLights && playOnceRight == 0){
       playOnceRight = 1;
+      wave(1);
     }
   }
 
-  if(command == 120){
-    // the arms have the control
+  // if the triangle button from the mobile app is pushed
+  // the command swiches from the wheels to the arms and viceversa
+  if(command == triangle){
     arms = !arms;
   }
 
   
 }
-
-// default = 83
-// dreapta = 82
-// stanga = 76
-// fata = 70
-// spate = 66
-// sus+stanga = 71
-// jos+stanga = 72
-// sus+dreapta = 73
-//jos+dreapta = 74
-// faruri fata = 119
-// faruri spate = 117
