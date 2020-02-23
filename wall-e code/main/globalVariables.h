b2dcf3ebbdc0af70d2fe3d45c956cb23f7053357
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2020-02-23 13:37:46 +0200

    add wheel control

--- wall-e code/main/globalVariables.h
+++ wall-e code/main/globalVariables.h
@@ -8,9 +8,16 @@
 Servo leftArm;
 Servo rightArm;
 
-
 int posServoRightArm = 0;
 int posServoLeftArm = 0;
 boolean arms = 1;
 
 boolean waveCheck = 0;
+
+AF_DCMotor motor1(1);
+AF_DCMotor motor4(3);
+
+unsigned long move_time = millis();
+
+int playOnceLeft = 0;
+int playOnceRight = 0;
--- wall-e code/main/wheel_movement.h
+++ wall-e code/main/wheel_movement.h
@@ -1,1 +1,45 @@
+# pragma once
+// move forword and backword with wall-e
 
+#include "globalVariables.h"
+
+
+
+
+void move_forward(int number){
+  if (number == 1){
+    motor1.run(FORWARD);
+    move_time = millis();
+  }
+
+  if(number == 2){
+    motor4.run(FORWARD);
+    move_time = millis();
+  }
+
+  if(move_time == 2000){
+    motor1.run(RELEASE);
+    motor4.run(RELEASE);
+    
+  }
+
+  
+}
+
+
+void move_backward(int number){
+  if (number == 1){
+    motor1.run(BACKWARD);
+    move_time = millis();
+  }
+
+  if(number == 2){
+    motor4.run(BACKWARD);
+    move_time = millis();
+  }
+
+  if(move_time == 2000){
+    motor1.run(RELEASE);
+    motor4.run(RELEASE);
+  }
+}
